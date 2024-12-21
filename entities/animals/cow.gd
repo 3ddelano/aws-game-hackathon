66176358f3c0ceaed9ec976ee80f3543bf8634b5
Delano Lourenco <3ddelano@gmail.com>
2024-12-21 13:11:34 +0530

    impl cow drops milk

--- entities/animals/cow.gd
+++ entities/animals/cow.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@export var milk_scene: PackedScene
+
+@onready var drop_marker: Marker2D = $DropMarker
+
+
+func _on_interactable_component_interacted() -> void:
+	var milk = milk_scene.instantiate()
+	milk.global_position = drop_marker.global_position
+	get_parent().add_child(milk)
