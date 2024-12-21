a4e05071b48fd41b1b303386064ace292a7f5f34
Delano Lourenco <3ddelano@gmail.com>
2024-12-21 13:07:06 +0530

    impl chicken drops egg

--- components/collectible_component.gd
+++ components/collectible_component.gd
@@ -12,6 +12,12 @@ func _ready() -> void:
 	if collectible_name == "":
 		push_error("Collectible name is required.")
 
+
+	monitoring = false
+	await get_tree().create_timer(0.2).timeout
+	monitoring = true
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print(&"Collected ", collectible_name)
--- entities/animals/chicken.gd
+++ entities/animals/chicken.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@export var egg_scene: PackedScene
+
+@onready var drop_marker: Marker2D = $DropMarker
+
+
+func _on_interactable_component_interacted() -> void:
+	var egg = egg_scene.instantiate()
+	egg.global_position = drop_marker.global_position
+	get_parent().add_child(egg)
