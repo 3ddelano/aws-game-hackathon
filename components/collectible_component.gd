06756502fa7d3367107d48384c562789b234bcad
Delano Lourenco <3ddelano@gmail.com>
2025-01-06 22:03:18 +0530

    fix time panel button texture, make item pickups reusable

--- components/collectible_component.gd
+++ components/collectible_component.gd
@@ -9,16 +9,15 @@ signal collected(item_data: SlotData)
 
 
 func _ready() -> void:
-	if not slot_data:
-		push_error("slot_data is required.")
-
-
 	monitoring = false
 	await get_tree().create_timer(0.2).timeout
 	monitoring = true
 
 
 func _on_body_entered(body: Node2D) -> void:
+	if not slot_data:
+		push_error("slot_data is required.")
+
 	if body is Player and body.inventory_data.pick_up_slot_data(slot_data):
 		print(&"Collected item=%s, count=%s" % [slot_data.item_data.name, slot_data.item_count])
 		collected.emit(slot_data)
