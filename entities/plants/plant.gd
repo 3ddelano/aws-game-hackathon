de0834dfc451a8e9405d2ea1b5d61bba6ced1334
Delano Lourenco <3ddelano@gmail.com>
2025-01-06 23:40:36 +0530

    added more crops

--- entities/plants/plant.gd
+++ entities/plants/plant.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var growth_frame_ids: Array[int]
+@export var use_sprites_list = false
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var growth_cycle_component: GrowthCycleComponent = $GrowthCycleComponent
@@ -15,7 +17,18 @@ func _ready() -> void:
 
 
 func _on_growth_stage_changed(stage_idx: int):
-	sprite_2d.frame = stage_idx
+	if use_sprites_list:
+		var sprites = get_node("Sprites")
+		for c in sprites.get_children():
+			c.hide()
+		sprites.get_child(stage_idx).show()
+		return
+	
+	
+	if len(growth_frame_ids) > 0:
+		sprite_2d.frame = growth_frame_ids[stage_idx]
+	else:
+		sprite_2d.frame = stage_idx
 
 
 func _on_interactable_component_interacted() -> void:
