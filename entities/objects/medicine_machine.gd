b0920da3b6427e3605cfdcbc96ec4dd2a80c95a1
Delano Lourenco <3ddelano@gmail.com>
2025-01-07 22:37:01 +0530

    impl ai response for make medicine

--- entities/objects/medicine_machine.gd
+++ entities/objects/medicine_machine.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 @export var inventory_data: InventoryData
 
 @onready var interactable_component: InteractableComponent = $InteractableComponent
+@onready var http_request: HTTPRequest = $HTTPRequest
 
 
 func _ready() -> void:
@@ -15,7 +16,66 @@ func _ready() -> void:
 func _on_interactable_component_interacted():
 	Events.toggle_external_inventory.emit(self)
 
+
 func _on_player_in_area_changed(player_in_area: bool):
-	print("player in area ", player_in_area)
 	if not player_in_area:
 		Events.clear_external_inventory.emit(self)
+
+
+func on_make_button_pressed():
+	var item_counts = {}
+	
+	for slot_data in inventory_data.slot_datas:
+		if slot_data and slot_data.item_data:
+			var item = slot_data.item_data
+			if item_counts.has(item):
+				item_counts[item] += slot_data.item_count
+			else:
+				item_counts[item] = slot_data.item_count
+	var items_str = ""
+	for item: ItemData in item_counts:
+		items_str += "- %s x%s\n" % [item.name, item_counts[item]]
+		
+	
+	var prompt = """
+You are a specialist organic medicine farmer.
+You are given:
+- a list of attributes and for each attribute you are given a list of items.
+- a disease, its description 
+- a list of available items
+
+Your task is to evaluate whether the disease can be cured using the list of items. Provide the output with yes or no. if no then give a short, concise, one or two line explanation.
+
+Attributes:
+- antibacterial: garlic
+- blood pressure-lowering: apple
+- pain relief: hot pepper
+- eye health: carrot
+- antioxidant: cucumber, tomato
+- hydrating: cucumber
+
+Disease: High fever - A high fever can be cured using an anti bacterial.
+
+Available Items:
+%s
+""" % [items_str]
+
+	print(prompt)
+	
+	for item: ItemData in item_counts:
+		inventory_data.consume_item(item)
+	
+	var output = await _get_ai_response(prompt)
+	
+	print("AI Response: ", output)
+	
+
+func _get_ai_response(prompt: String):
+	http_request.request("http://127.0.0.1:5000/ai-response", ["Content-Type: application/json"], HTTPClient.METHOD_POST, JSON.stringify({"prompt": prompt}))
+	var resp = await http_request.request_completed
+	var body_str = resp[3].get_string_from_utf8()
+	
+	var json = JSON.new()
+	json.parse(body_str)
+	var response = json.get_data()
+	return response["output"]
--- entities/player/inventory_manager.gd
+++ entities/player/inventory_manager.gd
@@ -4,8 +4,9 @@ extends Control
 @export var player: Player
 @export var player_inventory: Inventory
 @export var external_inventory: Inventory
-@onready var external_inventory_container: VBoxContainer = $ExternalInventoryContainer
+@export var make_button: Button
 
+@onready var external_inventory_container: VBoxContainer = $ExternalInventoryContainer
 @onready var grabbed_slot: Slot = $GrabbedSlot
 
 
@@ -21,6 +22,8 @@ func _ready() -> void:
 	
 	Events.toggle_external_inventory.connect(_on_toggle_external_inventory)
 	Events.clear_external_inventory.connect(_on_clear_external_inventory)
+	
+	make_button.pressed.connect(_on_make_button_pressed)
 
 
 func _process(delta: float) -> void:
@@ -81,3 +84,8 @@ func _on_inventory_slot_clicked(inv_data: InventoryData, slot_index: int, button
 func update_grabbed_slot():
 	grabbed_slot.visible = !!grabbed_slot_data
 	grabbed_slot.set_slot_data(grabbed_slot_data)
+
+
+func _on_make_button_pressed():
+	if external_inventory_owner and external_inventory_owner.has_method("on_make_button_pressed"):
+		external_inventory_owner.on_make_button_pressed()
